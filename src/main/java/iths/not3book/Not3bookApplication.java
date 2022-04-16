391db0af9de10bacab0a523f143dbc7855cb5cc9
PetterBT <petter.bergstrom@iths.se>
2022-04-16 17:24:41 +0200

    Initial commit

---
+++ src/main/java/iths/not3book/Not3bookApplication.java
@@ -0,0 +1,13 @@
+package iths.not3book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Not3bookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Not3bookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iths/not3book/Not3bookApplicationTests.java
@@ -0,0 +1,13 @@
+package iths.not3book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Not3bookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f59105f8a3cdd35c4fae84f1138e104b7fae59d7
PetterBT <petter.bergstrom@iths.se>
2022-04-28 19:32:42 +0200

    Add in-memory-user, passwordEncoder and configuration

--- src/main/java/iths/not3book/config/SecurityConfig.java
+++ src/main/java/iths/not3book/config/SecurityConfig.java
@@ -2,16 +2,40 @@ package iths.not3book.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
      @Bean
      public UserDetailsService userDetailsService() {
          var userDetailsService =
                  new InMemoryUserDetailsManager();
+
+         var user = User.withUsername("john")
+                 .password("12345")
+                 .authorities("read")
+                 .build();
+
+         userDetailsService.createUser(user);
           return userDetailsService;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+
     }
 }

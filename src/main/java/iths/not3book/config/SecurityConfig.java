8cc4495dac02d7803bbcc9f9913332a1e642f0ba
PetterBT <petter.bergstrom@iths.se>
2022-04-28 21:14:50 +0200

    Refactor SecurityConfig.java

--- src/main/java/iths/not3book/config/SecurityConfig.java
+++ src/main/java/iths/not3book/config/SecurityConfig.java
@@ -1,34 +1,31 @@
 package iths.not3book.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-     @Bean
-     public UserDetailsService userDetailsService() {
-         var userDetailsService =
-                 new InMemoryUserDetailsManager();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        var userDetailsService =
+                new InMemoryUserDetailsManager();
 
-         var user = User.withUsername("john")
-                 .password("12345")
-                 .authorities("read")
-                 .build();
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
 
-         userDetailsService.createUser(user);
-          return userDetailsService;
-    }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        userDetailsService.createUser(user);
+
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(
+                        NoOpPasswordEncoder.getInstance());
     }
 
     @Override
